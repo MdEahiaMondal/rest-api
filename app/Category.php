bee36b0fb20a2b977000bf5f3dc6d847a45eaf20
Eahiya <mdeahiamondal@gmail.com>
2020-07-05 10:45:29 +0600

    setup relationship

--- app/Category.php
+++ app/Category.php
@@ -10,4 +10,11 @@ class Category extends Model
         'name',
         'description'
     ];
+
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
+    }
+
 }
