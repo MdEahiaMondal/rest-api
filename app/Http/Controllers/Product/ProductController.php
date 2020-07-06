b8f9fd34bdd8700b061528c1a2ee29a435501acd
Eahiya <mdeahiamondal@gmail.com>
2020-07-06 13:55:56 +0600

    set product functionality

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -3,40 +3,28 @@
 namespace App\Http\Controllers\Product;
 
 use App\Http\Controllers\ApiController;
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends ApiController
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        //
+       $products = Product::all();
+       return $this->showAll($products);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+
+    public function show(Product $product)
     {
-        //
+        return $this->showOne($product);
     }
 
     /**
