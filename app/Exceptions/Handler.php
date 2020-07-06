a97d133d8037f862fa57db52f46d396d62da9ac0
Eahiya <mdeahiamondal@gmail.com>
2020-07-06 09:08:22 +0600

    QueryException error handel

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,6 +6,7 @@ use App\Traits\apiResponser;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -87,6 +88,18 @@ class Handler extends ExceptionHandler
         {
             return  $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
         }
+
+        // database related
+        if ($exception instanceof QueryException){
+            $error_code = $exception->errorInfo[1];
+            if ($error_code === 1451)
+            {
+                return  $this->errorResponse('Cannot remove this resource permanently.It is related with any other resource', 409);
+            }
+
+        }
+
+
         return parent::render($request, $exception);
     }
 
