5a57ae389a23c538d38807a1fdd1b8c718f8c34b
Eahiya <mdeahiamondal@gmail.com>
2020-07-07 15:25:58 +0600

    handling product availability using events with app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Product;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+
+       Product::updated(function ($product){ // it  will change when you need t o update you database row after successfully action always
+           if ($product->quantity === 0 && $product->isAvailable())
+           {
+               $product->status = Product::UNAVAILABLE_PRODUCT;
+               $product->save();
+           }
+       });
+
     }
 }
