f819621c8511d65cbf58e90c741036f74e6c88ce
Eahiya <mdeahiamondal@gmail.com>
2020-07-08 10:17:19 +0600

    set a name to application-header with middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\SignatureMiddleware;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -40,6 +41,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            'application_name:X-Application-Name',
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
@@ -63,5 +65,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'application_name' => SignatureMiddleware::class
     ];
 }
---
+++ app/Http/Middleware/SignatureMiddleware.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class SignatureMiddleware
+{
+
+    public function handle($request, Closure $next, $headerName = 'X-Header-Name')
+    {
+        $response = $next($request);
+        $response->headers->set($headerName, config('app.name'));
+        return $response;
+    }
+}
